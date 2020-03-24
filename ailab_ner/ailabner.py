fd6968e7b602b398cdcc2f88d8d2a8e2d7ceec60
Thiago Luis <thiagoluisrpinho@gmail.com>
2020-03-24 17:41:59 -0300

    :sparkles: feat: Function created successfully

--- ailab_ner/ailabner.py
+++ ailab_ner/ailabner.py
@@ -1,15 +1,27 @@
 from lenerbr.ner_model import NERModel
 from lenerbr.config import Config
-import pandas as pd
 from nltk import word_tokenize
 from nltk import data
 from nltk.tokenize.punkt import PunktSentenceTokenizer
+from typing import Set
+import pandas as pd
 import sys
 
-def entity_treatment(input_document: str) -> str:
+
+def entity_treatment(
+        input_document: str,
+        choosen_entities_types: Set[str] = set(["PESSOA", "LOCAL"]),
+        remove_entities: bool = False) -> str:
     ''' Receives an input string document with and
         returns a string with the choosen entity types
         replaced by tokens or deleted from the original text.
+        The available entity types to choose are:
+        PESSOA - For person names.
+        TEMPO - for time related terms.
+        LOCAL - For locations like countries, cities or states.
+        ORGANIZACAO - For enterprise names.
+        JURISPRUDENCIA - For jurisprudence related terms.
+        LEGISLACAO - For law related terms.
     '''
     tokenizer.train(input_document)
     doc_sentences = tokenizer.tokenize(input_document)
@@ -18,25 +30,23 @@ def entity_treatment(input_document: str) -> str:
         token_sentence = word_tokenize(sentence, language='portuguese')
         preds = model.predict(token_sentence)
         for index, word in enumerate(token_sentence):
-            ''' Removendo sufixo de tipo de entidades'''
+            ''' Removing entity types sufixes'''
             if preds[index][0:2] in ['B-', 'I-', 'E-', 'S-']:
                 preds[index] = preds[index][2:]
-            if preds[index] == 'PESSOA':
-                replaced_sentences.append('PESSOA_IDENTIFICADA')
-            elif preds[index] == 'LOCAl':
-                replaced_sentences.append('LOCALIDADE_IDENTIFICADA')
+            if preds[index] in choosen_entities_types:
+                if not remove_entities:
+                    replaced_sentences.append("TOKEN_" + str(preds[index]))
             else:
                 replaced_sentences.append(word)
     replaced_doc = " ".join(replaced_sentences)
     return replaced_doc
 
 
-# create instance of config
+''' Initializing model as one static variable. '''
 config = Config()
-
-# build model
-
 model = NERModel(config)
 model.build()
 model.restore_session(config.dir_model)
+
+''' Initializing tokenizer '''
 tokenizer = PunktSentenceTokenizer()
--- ailab_ner/tests/tests_NER.py
+++ ailab_ner/tests/tests_NER.py
@@ -14,3 +14,11 @@ def test_replace_entity_tokens(load_expected_io):
     input_document, expected_output = load_expected_io
     predicted_output = entity_treatment(input_document)
     assert predicted_output == expected_output
+
+
+@pytest.mark.parametrize(
+    'load_expected_io', [('input_1.txt', 'output_2.txt')], indirect=True)
+def test_remove_entity_tokens(load_expected_io):
+    input_document, expected_output = load_expected_io
+    predicted_output = entity_treatment(input_document, remove_entities=True)
+    assert predicted_output == expected_output
